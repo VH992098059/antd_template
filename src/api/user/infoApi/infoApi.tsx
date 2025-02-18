0d68b7eb224c2a950bf73c2e49532835d0db9848
VH992098059 <992098059@qq.com>
2025-02-19 00:45:58 +0800

    antd_template

---
+++ src/api/user/infoApi/infoApi.tsx
@@ -0,0 +1,12 @@
+import { message } from "antd";
+import api from "../../dataApi";
+
+export const InfoUser=async(userKey: object)=>{
+    try{
+    
+        const response=await api.get('/backend/info?user_key=User:'+userKey)
+        return response; // 返回成功状态
+    }catch(err){
+        message.error('请求失败，请稍后重试');
+    }
+}
